e8609caaf0d9d62aebafa4f1656300327868c0c6
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-02-28 07:39:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+/* Created by Tivotal */
+
+let btn = document.querySelector(".wrapper");
+
+btn.addEventListener("click", () => {
+  setTimeout(() => {
+    btn.classList.add("load");
+    btn.innerHTML = `<div class="loader"></div>`;
+  }, 1000);
+
+  setTimeout(() => {
+    btn.classList.add("check");
+    btn.innerHTML = `<div class="check"><i class="fas fa-check"></i></div>`;
+  }, 4500);
+
+  setTimeout(() => {
+    btn.className = "wrapper";
+    btn.innerHTML = `<div class="button anim">
+    <i class="fas fa-download"></i>
+    Download
+  </div>`;
+  }, 6000);
+});
